79918125fb5e274bb0fed8b9c7a1df9e0d821524
Angelina Wong <angelina.w25@gmail.com>
2018-05-02 19:58:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+  elsif int % 15 == 0
+    return "Fizzbuzz"
+  else 
+    return nil
 
 end
